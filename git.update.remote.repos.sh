e254205f8be1d246af4b2a23fa86a6aa2286270d
lothar-mar <lothar.mar@gmail.com>
2013-01-11 01:14:53 +0000

    Added initial script version.

---
+++ git.update.remote.repos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This shell script will update all git remote repositories in the machine automatically (using the fetch command).
+
+###############
+
+# Updating repos
+cd ~/local.repository/bash.scripts/git.update
+git push origin master
+
+cd ~/local.repository/bash.scripts/git.update
+git push origin master
+
+cd ~/local.repository/bash.scripts/system.update.remote.repos
+git push origin master
+
+cd ~/local.repository/bash.scripts/SpiderOak.maintenance
+git push origin master
