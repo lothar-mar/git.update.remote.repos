b69517845e5085b123555984d5c746e6682e4559
lothar-mar <lothar.mar@gmail.com>
2014-07-20 16:19:46 +0100

    Added support to vpn.scripts

--- git.update.remote.repos.sh
+++ git.update.remote.repos.sh
@@ -10,10 +10,10 @@ git push origin master
 cd ~/local.repository/bash.scripts/git.update.remote.repos
 git push origin master
 
-cd ~/local.repository/bash.scripts/system.update
+cd ~/local.repository/bash.scripts/SpiderOak.maintenance
 git push origin master
 
-cd ~/local.repository/bash.scripts/SpiderOak.maintenance
+cd ~/local.repository/bash.scripts/vpn.scripts
 git push origin master
 
 cd ~/local.repository/bash.scripts/get.quotes.yahoo
